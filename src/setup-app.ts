69cf3fb026af3c5e477f903ee354229bbb479c96
Гулевич Екатерина <meteorgul@gmail.com>
2025-04-21 17:28:02 +0300

    add cors

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,9 +2,23 @@ import express, { Express, Request, Response } from 'express';
 import { driversRouter } from './drivers/routers/drivers.router';
 import { testingRouter } from './testing/routers/testing.router';
 import { setupSwagger } from './core/swagger/setup-swagger';
+import cors from 'cors';
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
+
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:8000',
+        'http://localhost:4173',
+        'https://game-labels.vercel.app',
+        'https://game-labels-preview.vercel.app',
+      ], // Разрешить запросы с этого домена
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешить эти HTTP методы
+      credentials: true, // Разрешить отправку куки и заголовков авторизации
+    }),
+  );
 
   app.get('/', (req: Request, res: Response) => {
     res.status(200).send('hello world!!!');
